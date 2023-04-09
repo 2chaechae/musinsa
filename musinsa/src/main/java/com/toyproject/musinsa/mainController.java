9c7f0c4b662814371041154a40b0e35f79d99f31
chae_ <chae_@DESKTOP-9310PKD>
2023-04-09 14:09:05 +0900

    초기 셋팅

---
+++ musinsa/src/main/java/com/toyproject/musinsa/mainController.java
@@ -0,0 +1,16 @@
+package com.toyproject.musinsa;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class mainController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+		System.out.println("home controller start");
+                   
+        return "index";
+    }
+}
